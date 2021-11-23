bc4b9bdad05a79b404b819fd21cb1530848a7fda
cocoswebdevelop <scotomatey@gmail.com>
2021-11-23 11:16:41 +0100

    Removed an unnecessary  component || New scss class for global padding of components such as the calendar component

--- Front-End/src/app/modules/chat/routes.ts
+++ Front-End/src/app/modules/chat/routes.ts
@@ -1,14 +1,10 @@
 import { Routes } from "@angular/router";
-import { ChatBarComponent } from "./chat-bar/chat-bar.component";
+import { ChatBarComponent } from "../../ui-organisms/chat-bar/chat-bar.component";
 import { ChatComponent } from "./chat/chat.component";
 
 export const routes: Routes = [
     {
         path: 'chats',
         component: ChatComponent
-    },
-    {
-        path: 'chat-bar',
-        component : ChatBarComponent
     }
 ] 
